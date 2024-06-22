009f6bc2ed7e0af31e4738f036a3c3ce7c91e854
Mohammad Kaif <111599709+Mohammad092k@users.noreply.github.com>
2024-06-23 01:29:10 +0530

    Update atoms.js

--- src/atoms.js
+++ src/atoms.js
@@ -6,12 +6,10 @@ const localStorageEffect = key => ({ setSelf, onSet }) => {
         setSelf(JSON.parse(savedValue));
     }
 
-    onSet(newValue => {
-        //   if (newValue instanceof DefaultValue) {
-        //     localStorage.removeItem(key);
-        //   } else {
+    onSet(newValue => { 
+        
         localStorage.setItem(key, JSON.stringify(newValue));
-        //   }
+        
     });
 };
 
